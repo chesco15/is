8110efb817505b16398f6e82d0fd9d1d340275bb
chesco15 <i32orluf@uco.es>
2015-09-02 18:15:18 +0200

    clase Agenda

--- agenda.h
+++ agenda.h
@@ -24,56 +24,51 @@ typedef list<Contacto>::iterator Contacto_it;
 
 		public:
 
-			Agenda(GestorFichero * p): GestorDB(p){};
+			Agenda(GestorFichero * p): GestorDB(p){}; //constructor de agenda
 
-			std::string minuscula(std::string cadena){
-				std::locale loc;
-				std::string str=cadena;
-				for (std::string::size_type i=0; i<str.length(); ++i)
-	 				str[i] = std::tolower(str[i],loc);
+			string minuscula(string cadena){ //funcion que devuelve los apellidos en minuscula
+
+				locale loc;
+				string str=cadena;
+
+				for (string::size_type i=0; i<str.length(); ++i)
+	 				str[i] = tolower(str[i],loc);
 				return str;
 			}
 
 
 
-			list <Contacto> buscarApellido(std::string apellido){
+			list <Contacto> buscarApellido(string apellido){
+
 				//cargar del fichero
-				std::list<Contacto> auxiliar; //fichero completo hecho lista
+				list<Contacto> auxiliar; //fichero completo hecho lista
 				auxiliar = GestorDB->cargar();
-				std::string apellidos;
+				string apellidos;
 
 				list<Contacto> retorno; //lista
 
-				std::list<Contacto>::iterator i;
+				list<Contacto>::iterator i;
 
 				for (i = auxiliar.begin(); i != auxiliar.end(); i++){
 
 					apellidos=minuscula(i->getApellidos());//pasamos a minuscula los apellidos
 
-					std::size_t encontrado=apellidos.find(minuscula(apellido));//Buscamos el apellido en minuscula en los apellidos del contacto
+					size_t encontrado=apellidos.find(minuscula(apellido));//Buscamos el apellido en minuscula en los apellidos del contacto
 
-					if(encontrado!=std::string::npos){ //Si hay resultado se añadirá a la lista de retorno
+					if(encontrado!=string::npos){ //Si hay resultado se añadirá a la lista de retorno
 
 						retorno.push_back(*i); //iterator i es un puntero, por tanto para pasar su valor es necesario el operador *
 
-						//Incorporacion
-
-						//i->aumentaFrecuencia();
-						//this->modificar(i->getDni(),*i);//modificamos el contacto tras incrementar su frecuencia
-
-						//Fin de incorporacion
-
-						// COMENTARIOS: Para añadir funcionalidad de más frecuentes i->incrementarFrecuente();
 					}
 				}
 			return retorno;
 			}
 
-			bool buscarDni(std::string dni){
-				std::list<Contacto> auxiliar;
+			bool buscarDni(string dni){
+				list<Contacto> auxiliar;
 				auxiliar = GestorDB->cargar();
 
-				std::list<Contacto>::iterator i;
+				list<Contacto>::iterator i;
 				bool encontrado = false;
 
 				for (i = auxiliar.begin(); i != auxiliar.end(); i++){
@@ -81,8 +76,7 @@ typedef list<Contacto>::iterator Contacto_it;
 				if (i->getDni() == dni){
 
 					encontrado=true;
-					/* COMENTARIO: incluso se puede hacer el return aquí para no
-					 * seguir recorriendo la lista*/
+					
 					return encontrado;
 				}
 			}
@@ -94,7 +88,7 @@ typedef list<Contacto>::iterator Contacto_it;
 			
 			bool insertar(const Contacto &c) {
 
-				std::list<Contacto> auxiliar;
+				list<Contacto> auxiliar;
 				auxiliar = GestorDB->cargar();
 				auxiliar.push_back(c);
 
@@ -108,10 +102,10 @@ typedef list<Contacto>::iterator Contacto_it;
 			}
 
 
-			bool eliminar(std::string dni){
+			bool eliminar(string dni){
 
-				std::list<Contacto> auxiliar;
-				std::list<Contacto>::iterator i,j;
+				list <Contacto> auxiliar;
+				list <Contacto>::iterator i,j;
 				int encontrado;
 				auxiliar = GestorDB->cargar();
 
@@ -132,17 +126,18 @@ typedef list<Contacto>::iterator Contacto_it;
 			}
 
 
-			bool modificar(std::string dni, Contacto &c) {
-				std::list<Contacto> auxiliar;
+			bool modificar(string dni, Contacto &c) {
+
+				list<Contacto> auxiliar;
 				auxiliar = GestorDB->cargar();
-				std::list<Contacto>::iterator i;
+				list<Contacto>::iterator i;
 
 
 				for (i = auxiliar.begin(); i != auxiliar.end(); i++){
 
 					if (i->getDni() == dni){
 
-						*i=c; //iterator i es un puntero, por tanto para pasar su valor es necesario el operador *
+						*i=c; 
 						GestorDB->guardar(auxiliar);
 						return true;
 					}
@@ -150,7 +145,7 @@ typedef list<Contacto>::iterator Contacto_it;
 				return false;
 			}
 
-			std::list<Contacto> mostrarAgenda(){
+			list<Contacto> mostrarAgenda(){
 
 				return GestorDB->cargar();
 			}
